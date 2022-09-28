8417c529c368f4eada993da7b240aa7d15b17a67
SAEKay <114543365+SAEKay@users.noreply.github.com>
2022-09-28 11:59:23 +1000

    Started Week 2 Activities

---
+++ Assets/Scripts/ActivitiesWeek2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace KayBarhoum
+{
+
+    public class ActivitiesWeek2 : MonoBehaviour
+    {
+        private string favouriteGame = "Genshin Impact";
+        private float hoursPlayed = 1000;
+        private float costOfGame = 0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("my favourite game is " + favouriteGame + "!");
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+
+
+
+} 
